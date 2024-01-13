bdd1ab947e928ba83bcce9bf0be7f4c71be23627
kruti20 <kruti369@gmail.com>
2024-01-13 06:42:55 +0530

    Add-calendar-code

---
+++ resources/views/livewire/calendar.blade.php
@@ -0,0 +1,6 @@
+@include('users.header')
+<div>
+    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+    Testing Calendar event
+</div>
+@include('users.footer')
---
+++ resources/views/users/header.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Full Calendar</title>
+        <link rel="stylesheet" href="{{ asset('calendar/js/bootstrap-4.4.1.css') }}">
+        <link rel="stylesheet" href="{{ asset('calendar/css/header.css') }}">
+        <link rel="stylesheet" href="{{ asset('calendar/css/menu.css') }}">
+        <link rel="stylesheet" href="{{ asset('calendar/js/bootstrap-select.css') }}">
+        <link rel="stylesheet" href="{{ asset('calendar/css/common.css') }}">
+        <link rel="stylesheet" href="{{ asset('calendar/css/main.css') }}">
+        <link rel="stylesheet" href="{{ asset('calendar/css/my-calender/my-calender.css') }}">
+
+    </head>
+    <body>
